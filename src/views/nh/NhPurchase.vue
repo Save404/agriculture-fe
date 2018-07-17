b5f61b7c47e53b5b14644c63b98f63b3737ecbda
ZYSzys <17367077526@163.com>
2018-07-17 16:34:51 +0800

    simple markdown

--- src/views/nh/NhPurchase.vue
+++ src/views/nh/NhPurchase.vue
@@ -1,11 +1,22 @@
 <template>
-  <div>
+  <div class="components-container">
     <h2>原料购买</h2>
+    <div class="editor-container">
+      <markdown-editor id="contentEditor" ref="contentEditor" v-model="content" :height="400" :zIndex="20"></markdown-editor>
+    </div>
   </div>
 </template>
 
 <script>
+import MarkdownEditor from '@/components/MarkdownEditor'
+const content = `hello,world`
 export default {
-
+  components: { MarkdownEditor },
+  data() {
+    return {
+      content: content,
+      html: ''
+    }
+  }
 }
 </script>
