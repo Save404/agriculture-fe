fb113cafed8dfbc575f4f313a4be5f756f690822
ZYSzys <17367077526@163.com>
2018-07-27 21:54:55 +0800

    contract 15%

--- src/components/PSContract/index.vue
+++ src/components/PSContract/index.vue
@@ -1,21 +1,42 @@
 <template>
   <div class="pscontract-form">
-    <el-form :model="contractForm" ref="contractForm" label-width="80px">
-        <el-form-item label="支付宝">
-            <el-input v-model="contractForm.alipayAccount"></el-input>
-        </el-form-item>
-        <el-form-item v-for="(item, index) in contractForm" :label="index" :key="index">
-            <el-input v-model="contractForm[index]"></el-input>
-        </el-form-item>
-        <el-form-item>
-            <el-button type="primary" @click="onSubmit">立即创建</el-button>
-            <el-button>取消</el-button>
-        </el-form-item>
+    <h2>农产品购销合同书</h2>
+    <el-form :model="contractForm" ref="contractForm">
+      <el-form-item>
+        <MDinput name="name" v-model="contractForm.contractId" required :maxlength="100">
+          项目全称
+        </MDinput>
+      </el-form-item>
+      <el-row :gutter="0">
+        <el-col :span="12">
+          <el-form-item>
+            <MDinput name="name" v-model="contractForm.purchaserName" required :maxlength="100">
+              收购方
+            </MDinput>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item>
+            <MDinput name="name" v-model="contractForm.salesName" required :maxlength="100">
+              供货方
+            </MDinput>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-form-item v-for="(item, index) in contractForm" :label="index" :key="index">
+        <el-input v-model="contractForm[index]"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">立即创建</el-button>
+        <el-button>取消</el-button>
+      </el-form-item>
     </el-form>
   </div>
 </template>
 <script>
+import MDinput from '@/components/MDinput'
 export default {
+  components: { MDinput },
   data() {
     return {
       contractForm: {
@@ -39,4 +60,5 @@ export default {
     }
   }
 }
+
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -65,7 +65,19 @@ export const constantRouterMap = [{
       name: 'dashboard',
       meta: { title: '首页', icon: 'dashboard', noCache: true }
     }]
-  }
+  },
+  {
+    path: '/nh',
+    component: Layout,
+    meta: { roles: ['NH'] },
+    children: [{
+      path: 'contract',
+      name: 'PSContract',
+      component: () =>
+        import ('@/views/contract/index'),
+      meta: { title: '合同签订', icon: 'edit', roles: ['NH'] }
+    }]
+  },
 ]
 
 export default new Router({
@@ -183,7 +195,7 @@ export const asyncRouterMap = [{
       meta: { title: '农产品求购', icon: 'eye', roles: ['MJ'] }
     }]
   },
-  {
+  /*{
     path: '/nh',
     component: Layout,
     meta: { roles: ['NH'] },
@@ -194,22 +206,6 @@ export const asyncRouterMap = [{
         import ('@/views/contract/index'),
       meta: { title: '合同签订', icon: 'edit', roles: ['NH'] }
     }]
-  },
-  /*
-    {
-      path: '/example',
-      component: Layout,
-      redirect: '/example/list',
-      name: 'example',
-      meta: {
-        title: 'example',
-        icon: 'example'
-      },
-      children: [
-        { path: 'create', component: () => import('@/views/example/create'), name: 'createArticle', meta: { title: 'createArticle', icon: 'edit' }},
-        { path: 'edit/:id(\\d+)', component: () => import('@/views/example/edit'), name: 'editArticle', meta: { title: 'editArticle', noCache: true }, hidden: true },
-        { path: 'list', component: () => import('@/views/example/list'), name: 'articleList', meta: { title: 'articleList', icon: 'list' }}
-      ]
-    },*/
+  },*/
   { path: '*', redirect: '/404', hidden: true }
 ]
