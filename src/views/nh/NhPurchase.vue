8157d9dae0ec20e1bbb36c65a125d497c9edec2a
ZYSzys <17367077526@163.com>
2018-08-01 22:42:08 +0800

    resolve conflict

--- src/views/nh/NhPurchase.vue
+++ src/views/nh/NhPurchase.vue
@@ -9,25 +9,7 @@ export default {
   components: { PurchaseForm },
   data() {
     return {
-      //content: content,
-      html: '',
-      postForm: {
-        author: '17367077526',
-        telephone: '17367077526',
-        content: '',
-        basicId: '',//document.cookie.split(';')[5].split('=')[1],
-        title: '',
-        display_time: '',
-        importance: 0,
-        content_short: '',
-      },
-      rules: {},
-      contentShortLength: '',
-    }
-  },
-  methods: {
-    releasePurchase() {
-      addPurchase('NH', this.postForm)
+      type: 'NH'
     }
   }
 }
